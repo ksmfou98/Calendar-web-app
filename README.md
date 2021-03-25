db7282275d7e236630cccc7175d8a5e772bbd834
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-03-25 20:47:29 +0900

    컴포넌트 성능 최적화

---
+++ README.md
@@ -0,0 +1,5 @@
+# Calendar-web-app
+
+일정 관리 웹 어플리케이션 ( React )
+
+![화면-기록-2021-03-24-오후-10 28 30](https://user-images.githubusercontent.com/64634992/112318945-ebbcf100-8cf0-11eb-83b1-6b83892b3bb9.gif)
--- src/App.js
+++ src/App.js
@@ -3,24 +3,20 @@ import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoList from "./components/TodoList";
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "리액트의 기초 알아보기",
-      checked: true,
-    },
-    {
-      id: 2,
-      text: "컴포넌트 스타일링해 보기",
-      checked: true,
-    },
-    {
-      id: 3,
-      text: "일정 관리 앱 만들어 보기",
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
+
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos);
 
   const nextId = useRef(4);
 
@@ -31,28 +27,28 @@ const App = () => {
         text,
         checked: false,
       };
-      setTodos(todos.concat(todo));
+      setTodos(todos => todos.concat(todo));
       nextId.current += 1;
     },
-    [todos]
+    []
   );
 
   const onRemove = useCallback(
     (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
+      setTodos(todos => todos.filter((todo) => todo.id !== id));
     },
-    [todos]
+    []
   );
 
   const onToggle = useCallback(
     (id) => {
-      setTodos(
+      setTodos(todos =>
         todos.map((todo) =>
           todo.id === id ? { ...todo, checked: !todo.checked } : todo
         )
       );
     },
-    [todos]
+    []
   );
   return (
     <TodoTemplate>
